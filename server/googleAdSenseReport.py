0ace00a1de679e6e6ea4e397b835cf75f3608031
Seita Uchimura <c201552k@gmail.com>
2019-05-09 15:34:47 -0700

    :watermelon: Add:pythonサーバー実装

--- server/googleAdSenseReport.py
+++ server/googleAdSenseReport.py
@@ -4,24 +4,21 @@ from oauth2client.client import flow_from_clientsecrets
 from pytz import timezone
 import datetime
 import simplejson as json
+import pprint
 from oauth2client.file import Storage
 
 SCOPES = ['https://www.googleapis.com/auth/adsense.readonly']
 KEY_FILE_LOCATION = './keys/client_secrets.json'
-# VIEW_ID = '187987801'
-
-storage = Storage('credentials_file')
+storage = Storage('./keys/credentials_file')
 
 def initialize_adsense_reporting():
     flow = flow_from_clientsecrets('keys/client_secrets.json',
                                    SCOPES,
                                    redirect_uri='http://localhost:8080/')
-
     # auth_uri = flow.step1_get_authorize_url()
     # print(auth_uri)
-    code = "4/RQEKcXx6Ku4DFLNsl0Zy_PyBXrk9sg_LsTOiTezCmayBuZVt-mqpKoQlcGbmYVGilu9sOdT73fDOhMhOOpcjGFo"
+    # code = ""
     # credentials = flow.step2_exchange(code)
-    # print(credentials)
     # storage.put(credentials)
     credentials = storage.get()
     adsense = build('adsense', 'v1.4', credentials=credentials)
@@ -31,42 +28,36 @@ def get_report(adsense):
     now_pacific = datetime.datetime.now(timezone('US/Pacific'))
     today_pacific = now_pacific.strftime('%Y-%m-%d')
     
-    yesterday_pacific = now_pacific - datetime.timedelta(1)
+    yesterday_pacific = now_pacific - datetime.timedelta(6)
     yesterday_pacific = yesterday_pacific.strftime('%Y-%m-%d')
     
     return adsense.reports().generate(
-    startDate = today_pacific, 
-    endDate = yesterday_pacific,
+    startDate = yesterday_pacific, 
+    endDate = today_pacific,
     # filter=['AD_CLIENT_ID==' + ad_client_id],
-        metric=['PAGE_VIEWS', 'EARNINGS'],
-        dimension=['DATE'],
+        metric=['EARNINGS'],
+        dimension=['DATE', 'DOMAIN_NAME'],
         #sort=['+DATE']
     ).execute()
 
-adsense = initialize_adsense_reporting()
-print(adsense)
-# report = get_report(adsense)
-# print(report)
+def return_adsense_report():
+    adsense = initialize_adsense_reporting()
+    report = get_report(adsense)
+    earnings = {}
+    result = {}
+    for row in report.get('rows', []):
+        if row[1] == 'ucsb.tokyo':
+            earnings.update({ row[0]: row[2] })
+    totals = report.get('totals', [])
+    result.update({ 
+        'earnings': earnings, 
+        'total': totals[2] 
+    }) 
+    resp = Response()
+    resp.status_code = 500
+    resp.set_data(json.dumps(result))
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(result)
+    return resp
 
-
-# def return_analytics_report(response):
-    # result = {}
-    # resp = Response()
-    # for report in response.get('reports', []):
-        # columnHeader = report.get('columnHeader', {})
-        # dimensionHeaders = columnHeader.get('dimensions', [])
-        # metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
-    # for row in report.get('data', {}).get('rows', []):
-        # dimensions = row.get('dimensions', [])
-        # dateRangeValues = row.get('metrics', [])
-        # for header, dimension in zip(dimensionHeaders, dimensions):
-            # # print(header + ': ' + dimension)
-            # for i, values in enumerate(dateRangeValues):
-                # for metricHeader, value in zip(metricHeaders, values.get('values')):
-                    # # print(metricHeader.get('name') + ': ' + value)
-                    # if dimension == '(not set)':
-                        # dimension = 'Other'
-                    # result.update({ dimension: value })
-    # resp.status_code = 500
-    # resp.set_data(json.dumps(result))
-    # return resp
+return_adsense_report()
--- server/main.py
+++ server/main.py
@@ -1,19 +1,17 @@
 from flask import Flask, Response
 import simplejson as json
 from googleAnalyticsReport import return_analytics_report
+from googleAdSenseReport import return_adsense_report
 
 if __name__ == '__main__':
     app = Flask(__name__)
 
     @app.route('/googleAnalyticsReport', methods=['GET'])
-    def analyticsReport():
+    def googleAnalyticsReport():
         return return_analytics_report()
 
     @app.route('/googleAdSenseReport', methods=['GET'])
-    def adSenseReport():
-        analytics = initialize_analyticsreporting()
-        response = get_report(analytics)
-        return return_response(response)
-
+    def googleAdSenseReport():
+        return return_adsense_report()
 
     app.run('127.0.0.1', 8000, debug=True)
